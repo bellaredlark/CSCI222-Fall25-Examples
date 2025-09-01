cb1ed74fa3f07445fdb1e77eb71494b91db7d416
Bella Redlark <bredlark@ut.utm.edu>
2025-09-01 18:46:04 +0000

    hello, world!

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file hello.cpp
+ * @author Bella Redlark (bredlark@ut.utm.edu)
+ * @brief Hello World!
+ * @version 0.1
+ * @date 2025-09-01
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+ #include <iostream>
+ using namespace std;
+
+ int main() {
+    cout << "Hello, world" << endl;
+ }
